7a785c9aef89ad65112a8ddfcf58d0b8f22251f0
Ubuntu <pranav@caldera.xyz>
2023-10-30 17:13:57 +0000

    change WALLET_FACTORY, PAY_MASTER and reduce blocks to 100

--- core/state_transition.go
+++ core/state_transition.go
@@ -412,11 +412,11 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	// Building addresses from .env, at a later stage we prob should hardcode.
 	aaEntryPointEnvAddress := common.HexToAddress("0xfCe5B1d3c420D00DDF79C65189889aF3DD8e754f")
 	kintoIdEnvAddress := common.HexToAddress("0x8C63bBFec464133DA596084A4F16EA3081970dcB")
-	walletFactoryAddress := common.HexToAddress("0x4847bD5DC38194185579E3aE627140462D46Df00")
-	paymasterAddress := common.HexToAddress("0x199fa5B707aC410DC43035e20447E17D027684ef")
+	walletFactoryAddress := common.HexToAddress("0x6084DCe4587F49Ad5b5bfDc43531b86690fEdD38")
+	paymasterAddress := common.HexToAddress("0x3B760a9216E4adcA51dA7EB8b7B0F6D62288f9e6")
 
 	//First 1000 blocks allow us to deploy required contracts can be modified later
-	KINTO_RULES_BLOCK_START := big.NewInt(int64(1000))
+	KINTO_RULES_BLOCK_START := big.NewInt(int64(100))
 
 	destination := msg.To
 	currentBlockNumber := st.evm.Context.BlockNumber
